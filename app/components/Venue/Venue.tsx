5b2a3356a40e976ed5b70fbcad02ccd8756406c3
Jerom Miole <miolejerom@gmail.com>
2025-02-19 17:31:45 +1300

    Menu (#14)
    * adding location
    
    * footer finished
    
    * nav finish
    
    * polishing done
    
    * clearing up
    
    * menu and merch added
    
    * metadata
    
    * duplicate remove
    
    * remove unnecessary file
    
    * meta image
    
    * transfer metadata
    
    * polishing for BED
    
    * tweak some sections

--- app/components/Venue/Venue.tsx
+++ app/components/Venue/Venue.tsx
@@ -6,8 +6,8 @@ const Venue = () => {
   return (
     <section className="mx-auto lg:max-w-7xl">
       <div className="p-6 lg:p-24 gap-8 lg:gap-16 flex flex-col">
-        <div className="flex flex-1 flex-col gap-4 text-center">
-          <h2 className="font-superstar text-2xl lg:text-3xl text-blue">
+        <div className="flex flex-1 flex-col gap-4 text-center max-w-3xl mx-auto">
+          <h2 className="font-superstar text-2xl lg:text-5xl text-blue">
             Venue for Hire
           </h2>
           <p className="text-md lg:text-lg">
@@ -17,10 +17,10 @@ const Venue = () => {
             Americana atmosphere complete with neon lights, classic diner
             booths, and a jukebox playing timeless hits.
           </p>
-          <p className="text-lg md:text-2xl lg:text-2xl font-medium text-red">
+          <p className="mt-4 text-lg md:text-2xl lg:text-2xl font-medium text-red">
             As Featured In
           </p>
-          <p className="max-w-2xl mx-auto">
+          <p>
             Our diner has been the backdrop for incredible music videos, adding
             a nostalgic touch to creative projects. Check out these artists who
             filmed right here at B.E.D.!
